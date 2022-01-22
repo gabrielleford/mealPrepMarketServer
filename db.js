ed5aa7eec74dce67053fcc48533c68c454e52baf
Gabrielle Ford <gabrielledeva@gmail.com>
2022-01-22 12:27:37 -0500

    deploying to heroku

--- db.js
+++ db.js
@@ -1,7 +1,22 @@
 const { Sequelize } = require("sequelize");
 
 const db = new Sequelize(
-  `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/mealprepmarket`
+  process.env.DATABASE_URL
+    ||
+  `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/mealprepmarket`,
+  process.env.HOST == 'local' ?
+  {
+    dialect: 'postgres'
+  } : 
+  {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
 )
 
 module.exports = db;
