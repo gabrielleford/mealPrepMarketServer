5d5d309dc246d594c2a293ab672cc40441f194ad
Gabrielle Ford <gabrielledeva@gmail.com>
2022-02-06 19:05:05 -0500

    dropped tables

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use('/order', controllers.ordercontroller);
 
 
 dbConnection.authenticate()
-  .then(() => dbConnection.sync({force: true}))
+  .then(() => dbConnection.sync(/*{force: true}*/))
   .then(() => {
     app.listen(process.env.PORT, () => {
       console.log(`[Server]: App is listening on ${process.env.PORT}`);
