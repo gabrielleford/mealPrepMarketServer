c78efd6f4f68790f1aaa3698cf583f7c624fd778
Gabrielle Ford <gabrielledeva@gmail.com>
2022-02-09 19:09:36 -0500

    adding new header for react-admin

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+// app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 const dbConnection = require('./db');
 const controllers = require('./controllers');
 const middleware = require('./middleware');
