c447e4dfc0f24a723124fc9b8dbef79b08d5d6ae
Gabrielle Ford <gabrielledeva@gmail.com>
2022-02-09 21:10:36 -0500

    debug cors

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const cors = require('cors');
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+// const cors = require('cors');
+// app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(middleware.CORS);
 const dbConnection = require('./db');
 const controllers = require('./controllers');
