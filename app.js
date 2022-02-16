c4ebff7f29445357be95c2df8df8b243adfe1dcd
Gabrielle Ford <gabrielledeva@gmail.com>
2022-02-16 13:25:26 -0500

    debug

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: 'GET,POST,PUT,DELETE,HEAD',
+  methods: 'GET,POST,PUT,DELETE,HEAD,OPTIONS',
   allowedHeaders: 'Content-Type, Accept, Authorization, X-Requested-With, Origin',
 }))
 
